4a788de8693079d1ea28d795d711c0ff82cb1e8c
Bacoub12 <95268031+Bacoub12@users.noreply.github.com>
2022-01-08 15:30:13 -0500

    Basic Menu

---
+++ Assets/Scripts/SceneControl.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+   public void NextScene()
+   {
+        int indexScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(indexScene + 1);
+   }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0); 
+    }
+
+    public void LoadSetting()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
