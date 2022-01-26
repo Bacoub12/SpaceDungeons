5f3090fa3ff4d57477b3dd1ce8cafa9922e7e0fd
cr424 <73235268+cr424@users.noreply.github.com>
2022-01-26 15:17:48 -0500

    ennemi melee, fixes

--- Assets/Scripts/enemyMeleeScript.cs
+++ Assets/Scripts/enemyMeleeScript.cs
@@ -3,23 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class shotgunEnemyScript : MonoBehaviour
+public class enemyMeleeScript : MonoBehaviour
 {
-    public GameObject bullet;
-    public Transform shootPoint;
-
-    float lookRadius = 20f;
+    float lookRadius = 30f;
     Transform target;
     NavMeshAgent agent;
-    bool canShoot;
-    float cooldownLength = 3f;
+    bool canAttack;
+    float cooldownLength = 1f;
+    float attackRange = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.Find("PlayerCapsule").transform;
         agent = GetComponent<NavMeshAgent>();
-        canShoot = true;
+        canAttack = true;
     }
 
     // Update is called once per frame
@@ -32,7 +30,7 @@ public class shotgunEnemyScript : MonoBehaviour
             agent.SetDestination(target.position);
 
             Vector3 to = target.position + new Vector3(0f, 1f, 0f);
-            Vector3 from = shootPoint.position;
+            Vector3 from = transform.position;
             Vector3 direction = (to - from).normalized;
             Debug.DrawLine(from, to);
 
@@ -41,7 +39,7 @@ public class shotgunEnemyScript : MonoBehaviour
                 agent.SetDestination(transform.position);
                 FaceTarget(direction);
 
-                if (canShoot)
+                if (canAttack)
                 {
                     AttackTarget(direction);
                     StartCoroutine(shotCooldown());
@@ -52,40 +50,34 @@ public class shotgunEnemyScript : MonoBehaviour
 
     private void FaceTarget(Vector3 _direction)
     {
-        /*
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(_direction.x, 0, _direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
-        */
 
-        transform.LookAt(target);
+        //transform.LookAt(target);
     }
 
     private void AttackTarget(Vector3 _direction)
     {
-        //NOT SHOTGUN-LIKE YET
-        for (int i = 0; i < 40; i++)
+        if (Vector3.Distance(target.position, transform.position) < attackRange)
         {
-            float randomX = Random.Range(-20f, 20f);
-            float randomY = Random.Range(-20f, 20f);
-            float randomZ = Random.Range(-20f, 20f);
-            Rigidbody rb = Instantiate(bullet, shootPoint.position, Quaternion.identity).GetComponent<Rigidbody>();
-            rb.transform.forward = _direction;
-            rb.transform.Rotate(randomX, randomY, randomZ);
-            rb.AddForce(rb.transform.forward * 500f);
+            //deal damage to player
+            Debug.Log("attack");
         }
     }
 
     IEnumerator shotCooldown()
     {
-        canShoot = false;
+        canAttack = false;
         yield return new WaitForSeconds(cooldownLength);
-        canShoot = true;
+        canAttack = true;
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, lookRadius);
+        Gizmos.DrawWireSphere(transform.position, lookRadius); 
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
         /*
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, agent.stoppingDistance);
---
+++ Assets/Scripts/enemyShotgunScript.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyShotgunScript : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform shootPoint;
+
+    float lookRadius = 20f;
+    Transform target;
+    NavMeshAgent agent;
+    bool canShoot;
+    float cooldownLength = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("PlayerCapsule").transform;
+        agent = GetComponent<NavMeshAgent>();
+        canShoot = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            Vector3 to = target.position + new Vector3(0f, 1f, 0f);
+            Vector3 from = shootPoint.position;
+            Vector3 direction = (to - from).normalized;
+            Debug.DrawLine(from, to);
+
+            if (distance <= agent.stoppingDistance)
+            {
+                agent.SetDestination(transform.position);
+                FaceTarget(direction);
+
+                if (canShoot)
+                {
+                    AttackTarget(direction);
+                    StartCoroutine(shotCooldown());
+                }
+            }
+        }
+    }
+
+    private void FaceTarget(Vector3 _direction)
+    {
+        /*
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(_direction.x, 0, _direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        */
+
+        transform.LookAt(target);
+    }
+
+    private void AttackTarget(Vector3 _direction)
+    {
+        for (int i = 0; i < 40; i++)
+        {
+            float randomX = Random.Range(-20f, 20f);
+            float randomY = Random.Range(-20f, 20f);
+            float randomZ = Random.Range(-20f, 20f);
+            Rigidbody rb = Instantiate(bullet, shootPoint.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.transform.forward = _direction;
+            rb.transform.Rotate(randomX, randomY, randomZ);
+            rb.AddForce(rb.transform.forward * 500f);
+        }
+    }
+
+    IEnumerator shotCooldown()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(cooldownLength);
+        canShoot = true;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+        /*
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, agent.stoppingDistance);
+        */
+    }
+}
