743eca66faa35e129cd00e7e4f39c749b0f2e19e
VoSSis1 <49006747+VoSSis1@users.noreply.github.com>
2022-03-31 15:02:24 -0400

    Menu button cool sounds

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public AudioSource mySource;
+    public AudioClip click;
+    public AudioClip hover;
+
+    public void HoverSound()
+    {
+        mySource.PlayOneShot(hover);
+    }
+
+    public void ClickSound()
+    {
+        mySource.PlayOneShot(click);
+    }
+}
